1c1ea4f2550a1ff72d10432e1a29006474c99559
Adnan Hossain Addro <hello@codebyaddro.com>
2026-04-11 01:18:46 +0600

    user controller done
    auth middleware file renamed

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,9 @@
 import User from './../models/user.model.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const registerUser = async (req, res) => {
     try {
@@ -24,3 +29,44 @@ export const registerUser = async (req, res) => {
     }
 };
 
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+        )
+
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            user
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to login user",
+            error: error.message
+        })
+    }
+};
