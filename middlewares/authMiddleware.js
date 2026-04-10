892514708db88d6cddd26f197035c0bb13a75981
Adnan Hossain Addro <hello@codebyaddro.com>
2026-04-11 00:44:20 +0600

    auth middleware created

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,6 +11,11 @@ export const authMiddleware = (req, res, next) => {
         req.user = decoded.userId;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
+        return res.status(401).json(
+            { 
+                success: false,
+                message: 'Invalid token'
+            }
+        );
     }
 };
